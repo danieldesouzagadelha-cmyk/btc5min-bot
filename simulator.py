ce895ce2e46a69190d75e247e4c01d0e4a726120
danieldesouzagadelha-cmyk <danieldesouzagadelha@gmail.com>
2026-03-13 20:28:21 -0300

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,28 +1,39 @@
 capital_usdt = 50
 btc_balance = 0
 
+active_buys = set()
+
 def simulate(price, grid):
 
     global capital_usdt
     global btc_balance
+    global active_buys
 
     for level in grid:
 
-        if price <= level and capital_usdt >= 5:
+        # BUY
+        if price <= level and level not in active_buys and capital_usdt >= 5:
 
             btc_buy = 5 / price
             btc_balance += btc_buy
             capital_usdt -= 5
 
-            print("BUY SIMULADO:", price)
+            active_buys.add(level)
 
-        elif price >= level and btc_balance > 0:
+            print("BUY SIMULADO:", round(price,2))
 
-            usdt_sell = btc_balance * price
+        # SELL
+        if price >= level and level in active_buys:
+
+            btc_sell = btc_balance
+            usdt_sell = btc_sell * price
+
             capital_usdt += usdt_sell
             btc_balance = 0
 
-            print("SELL SIMULADO:", price)
+            active_buys.remove(level)
+
+            print("SELL SIMULADO:", round(price,2))
 
     total = capital_usdt + btc_balance * price
 
