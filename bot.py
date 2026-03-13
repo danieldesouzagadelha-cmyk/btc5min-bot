11e3c7901c7f1e21e830a9ba2f38a8e680b3c550
danieldesouzagadelha-cmyk <danieldesouzagadelha@gmail.com>
2026-03-13 06:40:53 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,54 @@
 import ccxt
 import time
-import os
 
-print("Bot iniciado...")
+print("Simulador de Grid iniciado")
 
-exchange = ccxt.mexc({
-    "enableRateLimit": True
-})
+exchange = ccxt.mexc()
 
 symbol = "BTC/USDT"
 
+capital_usdt = 50
+btc_balance = 0
+
+grid_lower = 64000
+grid_upper = 66000
+grid_levels = 10
+
+step = (grid_upper - grid_lower) / grid_levels
+
+grid = [grid_lower + step*i for i in range(grid_levels+1)]
+
+print("GRID:", grid)
+
 while True:
 
-    try:
+    ticker = exchange.fetch_ticker(symbol)
+    price = ticker["last"]
 
-        ticker = exchange.fetch_ticker(symbol)
+    print("Preço atual:", price)
 
-        price = ticker["last"]
+    for level in grid:
 
-        print("Preço BTC:", price)
+        if price <= level and capital_usdt > 5:
 
-    except Exception as e:
-        print("Erro:", e)
+            btc_buy = 5 / price
 
-    time.sleep(10)
+            btc_balance += btc_buy
+            capital_usdt -= 5
+
+            print("SIMULADO BUY:", price)
+
+        if price >= level and btc_balance > 0:
+
+            usdt_sell = btc_balance * price
+
+            capital_usdt += usdt_sell
+            btc_balance = 0
+
+            print("SIMULADO SELL:", price)
+
+    total = capital_usdt + btc_balance * price
+
+    print("Capital:", total)
+
+    time.sleep(20)
