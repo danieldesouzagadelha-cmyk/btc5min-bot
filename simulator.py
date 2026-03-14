9bff609ce6cedeb9f430709966b03657947022aa
danieldesouzagadelha-cmyk <danieldesouzagadelha@gmail.com>
2026-03-13 21:27:57 -0300

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,4 +1,5 @@
 import time
+from telegram_bot import send_message
 
 capital = 50
 btc = 0
@@ -8,24 +9,27 @@ position_price = None
 last_trade_time = 0
 cooldown_seconds = 60
 
+trades = 0
+wins = 0
+losses = 0
+
 def trade(price):
 
     global capital
     global btc
     global position_price
     global last_trade_time
+    global trades
+    global wins
+    global losses
 
     now = time.time()
 
-    # cooldown
     if now - last_trade_time < cooldown_seconds:
         print("Cooldown ativo")
         return
 
-    # ======================
     # BUY
-    # ======================
-
     if btc == 0 and capital >= 10:
 
         btc = 10 / price
@@ -36,26 +40,44 @@ def trade(price):
 
         print("BUY:", price)
 
+        send_message(f"🟢 BUY BTC\nPreço: {price}")
+
         return
 
-    # ======================
     # SELL
-    # ======================
-
     if btc > 0:
 
         profit = price - position_price
 
-        # take profit
-        if profit > 15:
+        if abs(profit) > 15:
 
             capital += btc * price
             btc = 0
 
+            trades += 1
+
+            if profit > 0:
+                wins += 1
+            else:
+                losses += 1
+
             last_trade_time = now
 
-            print("SELL:", price)
+            total = capital
 
-    total = capital + btc * price
+            msg = f"""
+🔴 SELL BTC
 
-    print("Capital:", round(total,2))
+Preço: {price}
+Lucro trade: {round(profit,2)}
+
+Trades: {trades}
+Wins: {wins}
+Losses: {losses}
+
+Capital: {round(total,2)}
+"""
+
+            print(msg)
+
+            send_message(msg)
