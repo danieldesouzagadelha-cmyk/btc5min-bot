030e5b7a6464c8ef45692fbfd859f47a19518c2d
danieldesouzagadelha-cmyk <danieldesouzagadelha@gmail.com>
2026-03-13 22:49:27 -0300

    Create mercado.py

---
+++ mercado.py
@@ -0,0 +1,17 @@
+import requests
+
+def get_market_data():
+
+    url = "https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=5"
+
+    data = requests.get(url).json()
+
+    bid = float(data["bids"][0][0])
+    ask = float(data["asks"][0][0])
+
+    bid_volume = float(data["bids"][0][1])
+    ask_volume = float(data["asks"][0][1])
+
+    price = (bid + ask) / 2
+
+    return price, bid, ask, bid_volume, ask_volume
