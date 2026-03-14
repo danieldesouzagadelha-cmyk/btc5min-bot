4f75f4ded991333185bb6109e7cfecc3316c3ca0
danieldesouzagadelha-cmyk <danieldesouzagadelha@gmail.com>
2026-03-14 15:16:18 -0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,9 +4,13 @@ import os
 BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
+
 def send_message(text):
 
+    print("Enviando mensagem para Telegram...")
+
     if not BOT_TOKEN or not CHAT_ID:
+        print("TOKEN ou CHAT_ID não encontrados")
         return
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -16,4 +20,6 @@ def send_message(text):
         "text": text
     }
 
-    requests.post(url, data=data)
+    response = requests.post(url, data=data)
+
+    print("Resposta Telegram:", response.text)
