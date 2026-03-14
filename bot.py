688a9adbbd2f990cf380785559b7aa272193f7f6
danieldesouzagadelha-cmyk <danieldesouzagadelha@gmail.com>
2026-03-14 14:15:04 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,57 @@
 import time
 from mercado import get_price
 from strategy import trade
+from telegram_bot import send_message
+
 
 pairs = [
-"BTCUSDT",
-"ETHUSDT",
-"SOLUSDT",
-"AVAXUSDT",
-"ADAUSDT",
-"LINKUSDT"
+    "BTCUSDT",
+    "ETHUSDT",
+    "SOLUSDT",
+    "AVAXUSDT",
+    "ADAUSDT",
+    "LINKUSDT"
 ]
+
 
 print("===================================")
 print(" MULTI COIN TREND PULLBACK BOT ")
 print("===================================")
 
+# envia mensagem quando inicia
+send_message("🤖 Bot iniciado e rodando na nuvem")
+
+
 loop = 0
 
 while True:
 
-    loop += 1
-    print("Loop:", loop)
+    try:
 
-    for pair in pairs:
+        loop += 1
+        print("Loop:", loop)
 
-        price = get_price(pair)
+        for pair in pairs:
 
-        trade(pair, price)
+            try:
 
-    time.sleep(1)
+                price = get_price(pair)
+
+                if price is None:
+                    print("Erro ao pegar preço:", pair)
+                    continue
+
+                trade(pair, price)
+
+            except Exception as e:
+                print("Erro na moeda", pair, e)
+
+        time.sleep(2)
+
+    except Exception as e:
+
+        print("Erro no loop principal:", e)
+
+        send_message(f"⚠️ Erro no bot: {e}")
+
+        time.sleep(5)
