ae39bc5968412142f5f77d14c38c2fe0cb65dece
danieldesouzagadelha-cmyk <danieldesouzagadelha@gmail.com>
2026-03-13 21:12:46 -0300

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,13 +1,26 @@
+import time
+
 capital = 50
 btc = 0
 
 position_price = None
+
+last_trade_time = 0
+cooldown_seconds = 60
 
 def trade(price):
 
     global capital
     global btc
     global position_price
+    global last_trade_time
+
+    now = time.time()
+
+    # verificar cooldown
+    if now - last_trade_time < cooldown_seconds:
+        print("Cooldown ativo")
+        return
 
     # BUY
     if btc == 0 and capital >= 10:
@@ -15,6 +28,8 @@ def trade(price):
         btc = 10 / price
         capital -= 10
         position_price = price
+
+        last_trade_time = now
 
         print("BUY:", price)
 
@@ -25,6 +40,8 @@ def trade(price):
 
             capital += btc * price
             btc = 0
+
+            last_trade_time = now
 
             print("SELL:", price)
 
